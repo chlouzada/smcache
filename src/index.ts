9f38ee01a91993adebcf32f42c02e3675127105b
chlouzada <chlouzada@gmail.com>
2023-03-06 22:20:31 -0300

    cache implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-export const hello = () => {
-  return 'world';
+type CacheItem = {
+  data: unknown;
+  expires: number;
 };
+
+const DEFAULTS = {
+  clone: true,
+  ttl: 1000 * 60 * 60 * 24,
+};
+
+type Options = {
+  clone?: boolean;
+  ttl?: number;
+};
+
+class SMCache {
+  #cache = new Map<string | number | object, CacheItem>();
+  readonly #clone: boolean;
+  readonly #ttl: number;
+
+  private constructor(opts?: Options) {
+    this.#clone = opts?.clone || DEFAULTS.clone;
+    this.#ttl = opts?.ttl || DEFAULTS.ttl;
+  }
+
+  #wrapper(data: unknown) {
+    const _data: unknown = this.#clone
+      ? JSON.parse(JSON.stringify(data))
+      : data;
+    return { data: _data, expires: Date.now() + this.#ttl };
+  }
+
+  get(key: string) {
+    const now = Date.now();
+    const item = this.#cache.get(key);
+    if (item && item.expires > now) {
+      return item.data;
+    }
+    this.#cache.delete(key);
+    return undefined;
+  }
+
+  set(key: string, data: unknown) {
+    this.#cache.set(key, this.#wrapper(data));
+  }
+
+  delete(key: string) {
+    this.#cache.delete(key);
+  }
+
+  static create(opts?: Options) {
+    return new SMCache(opts);
+  }
+}
+
+export const create = SMCache.create;
+export default SMCache.create();
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,40 @@
-import { test, assert } from 'vitest';
-import { hello } from '../src';
+import { test, expect, vi, beforeEach, describe } from 'vitest';
 
-test('test', () => {
-  assert.equal(hello(), 'world');
+import { create } from '../src';
+
+test('set and get', () => {
+  const cache = create();
+  const data = cache.get('key');
+  expect(data).toBeUndefined();
+  cache.set('key', 'value');
+  const value = cache.get('key');
+  expect(value).toBe('value');
+});
+
+describe('expiration', () => {
+  const opts = { ttl: 1000 * 60 * 60 * 24 };
+
+  let cache = create(opts);
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    cache = create(opts);
+  });
+
+  test('get an expired value', () => {
+    cache.set('key', 'value');
+
+    // expire the value using vitest
+    vi.advanceTimersByTime(1000 * 60 * 60 * 24 + 1);
+
+    const value = cache.get('key');
+    expect(value).toBeUndefined();
+  });
+
+  test('get a non-expired value', () => {
+    cache.set('key', 'value');
+
+    const value = cache.get('key');
+    expect(value).toBe('value');
+  });
 });
